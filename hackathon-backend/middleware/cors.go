053435b439e537864345b53eb49125a5a0e98329
Noa-Arai <noaarai3426@gmail.com>
2025-11-28 18:12:44 +0900

    COrsを編集

--- hackathon-backend/middleware/cors.go
+++ hackathon-backend/middleware/cors.go
@@ -7,12 +7,12 @@ import (
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		// CORS 設定
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods",
+			"GET, POST, PATCH, PUT, DELETE, OPTIONS")
 
-		// OPTIONS (プリフライト) はここで返す
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
